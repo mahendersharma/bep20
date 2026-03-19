aa7ca71d0952c86f1c7f9f7f4afff1aafd099029
Mahender Sharma <sharma27july1999@gmail.com>
2026-03-19 09:42:08 +0530

    first commit

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -279,7 +279,7 @@ export function Hero() {
         </div>
 
         <h1 className="text-4xl md:text-6xl font-bold text-white leading-tight">
-          Secure Your <span className="text-yellow-400">Digitalss Assets</span>
+          Secure Your <span className="text-yellow-400">Digital Assets</span>
         </h1>
 
         <button
